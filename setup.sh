f5442d271405824c344c771598c3423466d0e8f8
Luke <lukespinardi@gmail.com>
2017-11-24 11:47:17 -0600

    Fix sudo

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "" == "`which wget`" ]; 
-then echo "wget Not Found"; 
 if [ -n "`which apt-get`" ]; 
 then COMMAND="apt-get" ; 
 elif [ -n "`which yum`" ]; 
 then COMMAND="yum" ; 
-fi ; 
-fi
+fi 
 
 $COMMAND -y install wget
+
+if [ "yum" == "`$COMMAND`" ] && [ "" == "`which "Development Tools"`"];
+then `yum install -y "Development Tools"`;
+fi
+
 $COMMAND -y groupinstall "Development Tools"
 wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 tar -xzf Python-2.7.11.tgz
