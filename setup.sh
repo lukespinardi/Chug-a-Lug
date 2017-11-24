f3f8873999582cfa4cd693fb538addc4ebce0214
Luke <lukespinardi@gmail.com>
2017-11-24 11:22:36 -0600

    Updated Shell

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+if [ "" == "`which wget`" ]; then echo "Unzip Not Found"; if [ -n "`which apt-get`" ]; then apt-get -y install wget ; elif [ -n "`which yum`" ]; then yum -y install wget ; fi ; fi
+
 wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 tar -xzf Python-2.7.11.tgz
 cd Python-2.7.11
