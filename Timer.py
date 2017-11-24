17eb688edf723bb16f36f4327030e9d39cc4d7cd
Luke Spinardi <33501020+lukespinardi@users.noreply.github.com>
2017-11-24 09:12:25 -0600

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,28 @@
+import time
+
+class FollowTimer:
+    def __init__(self, _start=None):
+        self.elapsed = 0.0
+        self._start = _start
+
+    def startTime(self):
+        self._start = time.time()
+
+    def setTime(self, load):
+        self._start = load
+
+    def getTime(self):
+        return self._start
+
+    def getElapsed(self):
+        if self._start is None:
+            return 0
+        now = time.time()
+        self.elapsed = now - self._start
+        return self.elapsed
+
+    def running(self):
+        return self._start is not None
+
+    def getRemaining(self):
+        return 86400 - self.getElapsed()
