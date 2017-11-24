f0ab7675fa3213542ad9518a4b9257220956569d
Luke <lukespinardi@gmail.com>
2017-11-24 11:39:06 -0600

    Update for Development Tools

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-if [ "" == "`which wget`" ]; then echo "Unzip Not Found"; if [ -n "`which apt-get`" ]; then apt-get -y install wget ; elif [ -n "`which yum`" ]; then yum -y install wget ; fi ; fi
+if [ "" == "`which wget`" ]; 
+then echo "wget Not Found"; 
+if [ -n "`which apt-get`" ]; 
+then COMMAND="apt-get" ; 
+elif [ -n "`which yum`" ]; 
+then COMMAND="yum" ; 
+fi ; 
+fi
 
+$COMMAND -y install wget
+$COMMAND -y groupinstall "Development Tools"
 wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 tar -xzf Python-2.7.11.tgz
 cd Python-2.7.11
