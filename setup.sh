6d91df85a3ebcda57c060f525163c6b524c7df77
Luke <lukespinardi@gmail.com>
2017-11-24 12:28:35 -0600

    Script rehaul

--- setup.sh
+++ setup.sh
@@ -8,19 +8,22 @@ fi
 
 sudo COMMAND -y install wget
 
-if [ "yum" == "`$COMMAND`" ] && [ "" == "`which "Development Tools"`"];
-then `sudo yum install -y "Development Tools"`;
+if [ "yum" == "`$COMMAND`" ];
+	then `sudo yum install -y "Development Tools"`;
+	`sudo yum -y install epel-release`;
 fi
 
 $COMMAND -y groupinstall "Development Tools"
 
-wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
-tar -xzf Python-2.7.11.tgz
-cd Python-2.7.11
-./configure
-make 
-sudo make install
-export PATH=$PATH:/root/python-2.7.11
+if [ "" == "`which python2`"];then 
+	`wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz`;
+	`tar -xzf Python-2.7.11.tg`;
+	`cd Python-2.7.11`;
+	`./configure`;
+	`make`;
+	`sudo make install`;
+	`export PATH=$PATH:/root/python-2.7.11`;
+	fi
 
 sudo $COMMAND -y install python-pip
 sudo pip install selenium
