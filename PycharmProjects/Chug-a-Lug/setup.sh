f01136a12dc4370e292261b9b6f8bac0fb27f002
Luke <lukespinardi@gmail.com>
2017-11-24 11:13:17 -0600

    Remove weird folder

--- PycharmProjects/Chug-a-Lug/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
-tar -xzf Python-2.7.11.tgz
-cd Python-2.7.11
-./configure
-make 
-sudo make install
-export PATH=$PATH:/root/python-2.7.11
