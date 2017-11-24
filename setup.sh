24c4ac483fcbf6ba8645490885b042c9a9ee7645
Luke <lukespinardi@gmail.com>
2017-11-24 11:58:03 -0600

    Pip added to setup

--- setup.sh
+++ setup.sh
@@ -6,13 +6,14 @@ elif [ -n "`which yum`" ];
 then COMMAND="yum" ; 
 fi 
 
-$COMMAND -y install wget
+sudo COMMAND -y install wget
 
 if [ "yum" == "`$COMMAND`" ] && [ "" == "`which "Development Tools"`"];
-then `yum install -y "Development Tools"`;
+then `sudo yum install -y "Development Tools"`;
 fi
 
 $COMMAND -y groupinstall "Development Tools"
+
 wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 tar -xzf Python-2.7.11.tgz
 cd Python-2.7.11
@@ -20,3 +21,6 @@ cd Python-2.7.11
 make 
 sudo make install
 export PATH=$PATH:/root/python-2.7.11
+
+sudo $COMMAND -y install python-pip
+sudo pip install selenium
