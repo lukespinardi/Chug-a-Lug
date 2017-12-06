78e30f4ff4e1c2624f9601a4a42c6ac5acc8be62
Luke <lukespinardi@gmail.com>
2017-12-06 10:11:17 -0600

    CentOS setup redo

--- setup.sh
+++ setup.sh
@@ -1,39 +1,57 @@
 #!/usr/bin/env bash
 
-if [ -n "`which apt-get`" ]; 
-then COMMAND="apt-get" ; 
-elif [ -n "`which yum`" ]; 
-then COMMAND="yum" ; 
-fi 
-
-sudo COMMAND -y install wget
-
-if [ "yum" == "`$COMMAND`" ];
-	then sudo yum groupinstall -y "Development Tools";
-	`sudo yum -y install epel-release`;
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
 fi
 
+CentOSSetup ()
+{ # This script sets up Chug-A-Lug on Ubuntu
+sudo yum -y install wget python-pip tkinter tcl-devel tk-devel epel-release
+sudo yum -y groupinstall -y "Development Tools"
 if [ "" == "`which python2`" ];
-	then wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz;
-	`tar -xzf Python-2.7.11.tg`;
-	`cd Python-2.7.11`;
-	`./configure`;
-	`make`;
-	`sudo make install`;
-	`export PATH=$PATH:/root/python-2.7.11`;
-	`cd ..`;
-	fi
+        then wget --no-check-certificate https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz;
+        `tar -xzf Python-2.7.11.tg`;
+        `cd Python-2.7.11`;
+        `./configure`;
+        `make`;
+        `sudo make install`;
+        `export PATH=$PATH:/root/python-2.7.11`;
+        `cd ..`;
+        fi
+sudo pip install selenium
+wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
+rm geckodriver-v0.16.1-linux64.tar.gz
+}
 
-if [ "" == "`find get-pip.py`"]; then
-	`cd Python-2.7.11/Tools/scripts`;
-	`wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py`;
-	fi
-
-echo "--------------------------------------------------------"
-echo "Almost there!"
-echo "Issue the following commands to complete the install"
-echo "While still in the /Python2.7.*/Tools/scripts directory, run $ python get-pip.py"
-echo "sudo python -m  pip -U  install selenium"i
-echo "wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz"
-echo "Unzip geckodriver* using gzip -d or tar -xzvf";
-echo "Then chmod +x geckodriver";
+case $OS in
+	"CentOS Linux")
+		CentOSSetup
+	;;
+	Ubuntu)
+		echo "Hi!"
+	;;
+esac
